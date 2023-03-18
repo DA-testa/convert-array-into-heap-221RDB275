16cf9af93f25410f6a91927ccad3c69288d45671
Miks Matīss Liepiņš <126872246+221RDB275@users.noreply.github.com>
2023-03-18 18:25:33 +0000

    versija2

--- build_heap.py
+++ build_heap.py
@@ -39,7 +39,7 @@ def main():
     elif input_type == "F":
         file_name = input()
         path = "./test/" + file_name
-        with open(path, 'r', encoding = 'utf-8') as test
+        with open(path, 'r', encoding = 'utf-8') as test:
             n = int(test.readline())
             data = list(map(int, test.readline().split()))
             assert len(data) == n
