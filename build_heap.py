4c7a137044ab661d9903e09f44809173ff12c236
Miks Matīss Liepiņš <126872246+221RDB275@users.noreply.github.com>
2023-03-19 10:16:22 +0000

    versija5

--- build_heap.py
+++ build_heap.py
@@ -7,13 +7,13 @@ def build_heap(data):
     # try to achieve  O(n) and not O(n2)
     n = len(data)
     for i in range(n // 2, -1, -1):
-        sort_heap(data, i, n)
+        sort_heap(data, i, swaps)
     return swaps
 
 def sort_heap(data, i, swaps):
     n = len(data)
-    lChild = 2 * i
-    rChild = 2 * i + 1
+    lChild = 2 * i + 1
+    rChild = 2 * i + 2
     mz = i
     if lChild < n:
         mz = lChild
@@ -21,7 +21,9 @@ def sort_heap(data, i, swaps):
         mz = rChild
     if i != mz:
         swaps.append((i, mz))
+        data[i], data[mz] = data[mz], data[i]
         sort_heap(data, mz, swaps)
+        return
 
 def main():
     
@@ -53,11 +55,12 @@ def main():
 
     # TODO: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
+    if len(data) <= 4 * len(data):
 
     # output all swaps
-    print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
+        print(len(swaps))
+        for i, j in swaps:
+            print(i, j)
 
 
 if __name__ == "__main__":
